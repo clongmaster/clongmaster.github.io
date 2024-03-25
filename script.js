170d5b06f6a1d26513f86bafba5b8244e6c33387
turtlethefish <turtlethefish@proton.me>
2024-03-25 22:02:43 +0400

    Added function to editGenInfo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var modal;
 // };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onmousedown = function (event) {
   var modals = document.getElementsByClassName("modal");
   for (let i = 0; i < modals.length; i++) {
     if (event.target == modals[i]) {
@@ -21,4 +21,34 @@ function openModal(modalId) {
 function closeModal(modalId) {
   modal = document.getElementById(modalId);
   modal.style.display = "none";
+}
+
+function saveGenInfo() {
+  var name = document.getElementById("nameInput").value;
+  var eng = document.getElementById("engNameInput").value;
+  var desc = document.getElementById("descInput").value;
+
+  let genInfo = {
+    name: name,
+    eng: eng,
+    desc: desc
+  }
+
+  let genInfo_serialized = JSON.stringify(genInfo);
+  localStorage.setItem("clongGenInfo", genInfo_serialized);
+
+  loadData();
+  closeModal("editGenInfo");
+}
+
+function loadData() {
+  let genInfo = JSON.parse(localStorage.getItem("clongGenInfo"));
+  
+  document.getElementById("name").innerHTML = genInfo.name;
+  document.getElementById("engName").innerHTML = genInfo.eng;
+  document.getElementById("desc").innerHTML = genInfo.desc;
+}
+
+window.onload = function() {
+  loadData();
 }
