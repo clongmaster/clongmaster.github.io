a66ede8f8a07ee8a573630fa270f6ca6e9b0bd4f
turtlethefish <turtlethefish@proton.me>
2024-03-28 00:00:49 +0400

    Added functionality to vowels table

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var modal;
+
 // When the user clicks on <span> (x), close the modal
 // span.onclick = function () {
 //   modal.style.display = "none";
@@ -21,6 +22,7 @@ function openModal(modalId) {
 function closeModal(modalId) {
   modal = document.getElementById(modalId);
   modal.style.display = "none";
+  loadData();
 }
 
 function saveGenInfo() {
@@ -37,12 +39,40 @@ function saveGenInfo() {
   let genInfo_serialized = JSON.stringify(genInfo);
   localStorage.setItem("clongGenInfo", genInfo_serialized);
 
-  loadData();
   closeModal("editGenInfo");
+}
+
+function select(cell) {
+  let values = cell.id.split('');
+  let i = parseInt(values[1]);
+  let j = parseInt(values[2]);
+  let arrName;
+
+  if (values[3] == 'r') {
+    arrName = "roundedVowels";
+  } else {
+    arrName = "unroundedVowels";
+  }
+  let arr = JSON.parse(localStorage.getItem(arrName));
+
+  if (cell.classList.contains("selected")) {
+    // If the cell already has the 'selected' class, remove it
+    cell.classList.remove("selected");
+    arr[i][j] = 0;
+  } else {
+    // If the cell doesn't have the 'selected' class, add it
+    cell.classList.add("selected");
+    arr[i][j] = 1;
+  }
+
+  arr_serialized = JSON.stringify(arr);
+  localStorage.setItem(arrName, arr_serialized);
 }
 
 function loadData() {
   let genInfo = JSON.parse(localStorage.getItem("clongGenInfo"));
+  let roundedVowels = JSON.parse(localStorage.getItem("roundedVowels"));
+  let unroundedVowels = JSON.parse(localStorage.getItem("unroundedVowels"));
 
   if (genInfo != null) {
     if (genInfo.name != "") {
@@ -64,11 +94,61 @@ function loadData() {
     document.getElementById("nameInput").value = genInfo.name;
     document.getElementById("engNameInput").value = genInfo.eng;
     document.getElementById("descInput").value = genInfo.desc;
+
+    for (let i = 0; i < unroundedVowels.length; i++) {
+      let count = 0;
+      for (let j = 0; j < unroundedVowels[i].length; j++) {
+        if (unroundedVowels[i][j] != 0) {
+          let element = document.getElementById(`vowel${i}${j}u`);
+          let cell = document.getElementById(`v${i}${j}u`);
+          count += 1;
+          if (element != null) {
+            element.style.visibility = "visible";
+            if (!cell.classList.contains("selected")) {
+              cell.classList.add("selected");
+            }
+          }
+        } else {
+          let element = document.getElementById(`vowel${i}${j}u`);
+          let cell = document.getElementById(`v${i}${j}u`);
+          if (element != null) {
+            element.style.visibility = "hidden";
+            if (cell.classList.contains("selected")) {
+              cell.classList.remove("selected");
+            }
+          }
+        }
+        if (roundedVowels[i][j] != 0) {
+          let element = document.getElementById(`vowel${i}${j}r`);
+          let cell = document.getElementById(`v${i}${j}r`);
+          count += 1;
+          if (element != null) {
+            element.style.visibility = "visible";
+            if (!cell.classList.contains("selected")) {
+              cell.classList.add("selected");
+            }
+          }
+        } else {
+          let element = document.getElementById(`vowel${i}${j}r`);
+          let cell = document.getElementById(`v${i}${j}r`);
+          if (element != null) {
+            element.style.visibility = "hidden";
+            if (cell.classList.contains("selected")) {
+              cell.classList.remove("selected");
+            }
+          }
+        }
+      }
+      if (count > 0) {
+        document.getElementById(`vowelRow${i + 1}`).style.display = "table-row";
+      }
+    }
   }
 }
 
-function playAudio(audio) {
-  document.getElementById(audio).play()
+function playAudio(event, audio) {
+  document.getElementById(audio).play();
+  event.stopPropagation();
 }
 
 window.onload = function () {
